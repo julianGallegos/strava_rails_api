4c569be050425cf834349b389a5c8d8fff3f89a6
julian <julian.gallegos@gmail.com>
2015-02-20 23:23:59 -0800

    able to return coordinates from run activity

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,11 +8,32 @@ def index
 
 	token = session[:access_token]
 
+	user_id = session[:uid]
+
 	@client = Strava::Api::V3::Client.new(:access_token => token)
 
-	@activties =	@client.list_athlete_activities
+	@current_athlete = @client.retrieve_current_athlete
+
+	@runs = @client.list_athlete_activities
+	
+	activity_id = params[:activity_id]
+
+
+
+	headers = {"Content-type"=> "application/json", "Authorization"=> "Bearer d2e2f6ce8fbf2f0ade6adf4347ea791c0cf8bfcf"}
+
+	p @coordinates = HTTParty.get("https://www.strava.com/api/v3/activities/257526140/streams/latlng", :headers => headers)
+
+	debug
+
+
+
+
+	# @last_run = HTTParty.get('https://www.strava.com/api/v3/activities/57526140/streams/latlng')
+
 
 	
+
 end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,44 +2,20 @@
 class SessionsController < ApplicationController
 
 
-
-# def request_access
-
-	# @code=params["code"];
-	#      @response_temp=HTTParty.post("https://www.strava.com/oauth/token",:body=>{
-	#     :client_id => '4820',
-	#     :client_secret => '5a016a1216d550e6811e746f68fce90ff53851a9',
-	#     :code=>@code,
-	#     :redirect_uri=>"https://localhost:3000"
-	#     }.to_json,
-	#     :headers => {'Content-Type'=>'application/json'})
-	#     p @response_temp
-	#     access_token=@response_temp.parsed_response["access_token"]
-	
-
-
-
-	# url = "https://www.strava.com/oauth/authorize?client_id=4820&response_type=code&redirect_uri=http://localhost:3000&approval_prompt=force"
-
-	# @response = HTTParty.post(url)
-
-	# render json: @response
 	
 def create
-    # @user = User.find_or_create_from_auth_hash(auth_hash)
-    # self.current_user = @user
+
     auth = request.env["omniauth.auth"]
 
     access_token = auth.credentials.token
     name = auth.info.name
-    uid = auth.info.uid
-    nickname = auth.info.nickname
+    uid = auth.uid
+    
 
     session[:access_token] = access_token
     session[:name] = name
-    session[:uid] = uid
-    session[:nickname] = nickname
-
+    p session[:uid] = uid
+ 
     puts "The access token is #{access_token}"
 
     redirect_to search_path

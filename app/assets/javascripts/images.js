0838e72aae6295a174ade9412128acd4c7601f24
julian <julian.gallegos@gmail.com>
2015-02-25 22:45:19 -0800

    left comments for next steps to take with id returned from run

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -24,10 +24,15 @@ function Controller(view){
 	this.view = view;
 }
 
+//use this getRunId method as paramtes to search for the strava run coordinates
+//enter these coorindates as a parameter and should hit the search#search_runs method
+// after the coordinates are returned, those will be entered into instgram to get the image objects
 Controller.prototype.searchForImages = function(){
 	console.log("controller search method")
 	this.view.getRunId()
 }
+
+
 
 Controller.prototype.bindEventListeners = function(){
 	$(this.view.runIds).on('click', this.searchForImages.bind(this))

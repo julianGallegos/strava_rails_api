0574e70860514cc5a26f39e2d9f27a983e9d79e0
julian <julian.gallegos@gmail.com>
2015-02-25 22:42:52 -0800

    javascript file returns id of run when clicked

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -9,9 +9,13 @@
 //======================view ===========================
 
 function View(){
-
+	this.runIds = ".runIds"
 }
 
+View.prototype.getRunId = function(){
+	var runIdClicked = event.target.id
+	console.log(runIdClicked)
+}
 
 
 //======================controller=======================
@@ -20,11 +24,17 @@ function Controller(view){
 	this.view = view;
 }
 
+Controller.prototype.searchForImages = function(){
+	console.log("controller search method")
+	this.view.getRunId()
+}
+
 Controller.prototype.bindEventListeners = function(){
-	
+	$(this.view.runIds).on('click', this.searchForImages.bind(this))
 }
 
 $(document).ready(function(){
-	var myApp = new Controller(new view())
-	myApp.bindEventListeners();
+	var viewFromMyRun = new Controller(new View());
+	viewFromMyRun.bindEventListeners()
+
 })
